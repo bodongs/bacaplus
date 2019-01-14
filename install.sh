aca15d4e27ca13db7b3cb92838d90295bcf533be
bodongs <46390411+bodongs@users.noreply.github.com>
2019-01-14 16:37:13 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+apt update && apt upgrade
+apt install unzip
+pkg install php
+unzip baca.zip
+sleep 2
+echo "sukses terpasang botnya"
+sleep 2
+echo "untuk menjalankan ketik"
+sleep 2
+echo "cd baca"
+sleep 2
+echo "php bot.php
+echo "pantau terus channel Airdop Alarm Gan 😂"
+rm -rf install.sh
